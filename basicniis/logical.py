c662b5eaa5436a58cb1d0a5c98f00f57983ede6b
unknown <ompalai1234@gmail.com>
2026-02-23 10:40:57 +0530

    test

---
+++ basicniis/logical.py
@@ -0,0 +1,14 @@
+#wap take rectangle length and breadth form keyboard display length breadth area and parameter
+print("Enter rectangle length :")
+l=int(input())
+print("Enter rectangle breadth :")
+b=int(input())
+ar=l*b
+pr=2*(l+b)
+print("length =" ,l)
+print("breadth =", b)
+print("area =" ,ar)
+print("parameter =" ,pr)
+
+
+
---
+++ basicniis/square.py
@@ -0,0 +1,7 @@
+print("enter squre length :")
+l=int(input())
+print("side",l)
+a=l*l
+p=l*4
+print("area=",a)
+print("parameter=",p)
